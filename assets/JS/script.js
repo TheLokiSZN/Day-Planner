227596c3a92281107c9733dd38154fca85c8f323
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-08-12 22:46:07 -0400

    updated psuedo code for js

---
+++ assets/JS/script.js
@@ -0,0 +1,76 @@
+// create our document ready function to make sure nothing runs before we load the page.
+$(document).ready(function () {
+
+// function to listen for click events on the page
+//onclick function() {
+
+
+    // grab the save event options
+        // create variables to save user input and time
+    var
+    var
+    // save to localStorage
+
+    // alert user that items have been saved to localStorage.
+
+    // create some kind of timeOut value that removes the notification
+
+
+//}
+
+
+
+// create time updater function()
+    // get the current time
+
+    // create our loop to go over all the time blocks
+
+    // if else statement to
+    // check to see if we have passed our time
+        // check out removeClass and addClass
+        // ().removeClass()
+    // check to see if current hour matches physical hour (actual time/html)
+
+    // check to see if the time is in the future
+
+
+
+
+// retrieve localStorage and render items to the correct rows
+
+//get one working then copy paste and change the values
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
